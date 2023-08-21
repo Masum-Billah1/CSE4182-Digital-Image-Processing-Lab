4247f8e6f0f0f3716e840a458397c8119d502512
Masum Bilah <53857306+Masum-Billah1@users.noreply.github.com>
2023-08-21 16:43:47 +0600

    Create imagesplit_vertical.py

---
+++ imagesplit_vertical.py
@@ -0,0 +1,10 @@
+import cv2
+image = cv2.imread('Images/resized_rose.jpg')
+height, width, channels = image.shape
+half = width//2
+left = image[:, :half]
+right = image[:, half:]
+
+cv2.imshow('Right',right)
+cv2.imshow('Left',left)
+cv2.waitKey(0)
