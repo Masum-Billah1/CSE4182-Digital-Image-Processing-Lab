9413a53e255f5b15eb903af229ee16e643b365cc
Masum Bilah <53857306+Masum-Billah1@users.noreply.github.com>
2024-01-01 23:16:10 +0600

    Decrease intensity level resolution

--- Task_1_b.py
+++ Task_1_b.py
@@ -1,4 +1,6 @@
 import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
 
 def IntensityResolution(N):
     div = int(256/N)
@@ -9,13 +11,16 @@ def IntensityResolution(N):
             img[i,j] = levels*div
     return img
 
-img = cv.imread('images/gray_rose.jpg',0)
-[row,col] = img.shape
+img = cv.imread('images/gray_ros.jpg',0)
 
+row,col = img.shape
 gray_level = 256
 for i in range(8):
-    print(gray_level)
     new_img = IntensityResolution(gray_level)
     gray_level //= 2
-    cv.imshow('Rose', new_img)
-    cv.waitKey(0)
+    plt.subplot(3,3,(i+1))
+    plt.title(f'Gray level: {int(np.log2(gray_level)+1)}')
+    plt.imshow(new_img,cmap='gray')
+    plt.axis('off')
+
+plt.show()
