d83d8b9fc47d83d6038778a218c67233dc5bdd90
Masum Bilah <53857306+Masum-Billah1@users.noreply.github.com>
2024-01-02 18:47:07 +0600

    brightness enhancement

--- Task_2_a.py
+++ Task_2_a.py
@@ -3,13 +3,22 @@ import matplotlib.pyplot as plt
 img = cv.imread('Images/gray_rose.jpg',0)
 row,col = img.shape
 
+enhanced_image = img
 for i in range(row):
     for j in range(col):
-        if(img[i][j]>=64 and img[i][j]<128):
-            img[i][j]+=100
-        if(img[i][j]>255):
-            img[i][j] = 255
+        if(enhanced_image[i][j]>=64 and enhanced_image[i][j]<128):
+            enhanced_image[i][j]+=100
+        if(enhanced_image[i][j]>255):
+            enhanced_image[i][j] = 255
 
-plt.title('Brightness Enhancement Image')
+
+plt.subplot(1,2,1)
 plt.imshow(img,cmap='gray')
+plt.title('Original Image')
+plt.axis('off')
+
+plt.subplot(1,2,2)
+plt.axis('off')
+plt.imshow(img,cmap='gray')
+plt.title('Brightness Enhancement Image')
 plt.show()
