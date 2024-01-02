7b3ee2c2df2fdebbe4e55e093fb92aaebab54e0e
Masum Bilah <53857306+Masum-Billah1@users.noreply.github.com>
2024-01-02 18:35:06 +0600

    Histogram & Thresholding

--- Task_1_c.py
+++ Task_1_c.py
@@ -2,9 +2,32 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv.imread('Images/gray_rose.jpg',0)
-row,col = img.shape
+def Basic_global_thresholding(image):
+    temp1 = len(frequency)//2
+    temp2 = 0
+    T_inf = 1e-5
 
+    while(1):
+        temp2 = temp1
+        G1 = image > temp1
+        G2 = image <= temp1
+
+        mu1 = np.mean(image[G1])
+        mu2 = np.mean(image[G2])
+
+        temp1 = (mu1 + mu2) / 2
+
+        if abs(temp2- temp1) < T_inf:
+            break
+
+    return  temp1
+
+
+img = cv.imread('Images/lena.jpg',0)
+row,col = img.shape
+plt.subplot(2,1,1)
+plt.imshow(img,cmap='gray')
+plt.title('Original image')
 frequency = np.zeros(256)
 
 for i in range(row):
@@ -12,19 +35,25 @@ for i in range(row):
         frequency[img[i,j]] += 1
 
 #Histogram
+plt.subplot(2,2,3)
 plt.bar(range(256), np.array(frequency))
 
 plt.xlabel('Intensity')
 plt.ylabel('Frequency')
 plt.title('Histogram')
+
+thresold_frequency = np.zeros(256)
+Final_Threshold = Basic_global_thresholding(img)
+for i in range(255):
+    if(i<Final_Threshold):
+        thresold_frequency[0] += frequency[i]
+    else:
+        thresold_frequency[255] +=frequency[i]
+
+plt.subplot(2,2,4)
+plt.bar(range(256), np.array(thresold_frequency),width=2)
+plt.xlabel('Intensity')
+plt.ylabel('Frequency')
+plt.title('Histogram after Single Treshold')
 plt.show()
-
-for i in range(row):
-    for j in range(col):
-        if img[i,j] >= 100:
-            img[i,j] = 255
-        else:
-            img[i,j] = 0
-
-cv.imshow('Figure 2',img)
-cv.waitKey(0)
+plt.close()
