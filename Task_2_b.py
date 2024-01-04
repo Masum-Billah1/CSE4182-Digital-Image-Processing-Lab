5d92a5b1cfbfe31adbaf87885eb312d5178a52ab
Masum Bilah <53857306+Masum-Billah1@users.noreply.github.com>
2024-01-04 12:22:21 +0600

    Power Law, Logarithmic and Inverse Logarithmic Transformation

--- Task_2_b.py
+++ Task_2_b.py
@@ -5,28 +5,35 @@ import matplotlib.pyplot as plt
 
 img = cv.imread('images/night.png',0)
 img = cv.resize(img,(512,512))
-original_img = img
+original_img = img.copy()
 row,col = img.shape
 
 gama = 0.6
 
+# c is the scaling factor. It is considered as constant also
+# main equation: s = c*(1+r). c is calculated for maximum value of input and output
 c = 255/ (np.log(1+np.max(img)))
 power_img = np.zeros((row, col))
+logarithomic_img = np.zeros((row, col))
 inverse_img = np.zeros((row, col))
 
 for i in range(row):
     for j in range(col):
         power_img[i,j] = (img[i,j]**gama)
-        inverse_img[i,j] = np.exp(img[i,j] / c) - 1
-        # inverse_img[i][j] = np.exp(img1[i][j])**(1/c) - 1
+        logarithomic_img[i,j] = c*np.log(1+img[i,j])
+        inverse_img[i, j] = np.exp(logarithomic_img[i, j] / c) - 1
 
-plt.subplot(1,3,1)
+
+plt.subplot(1,4,1)
 plt.imshow(original_img,cmap='gray')
 plt.title('Original Image')
-plt.subplot(1,3,2)
+plt.subplot(1,4,2)
 plt.imshow(power_img,cmap='gray')
-plt.title('Original Image')
-plt.subplot(1,3,3)
+plt.title('Power Law Image')
+plt.subplot(1,4,3)
+plt.imshow(logarithomic_img,cmap='gray')
+plt.title('logarithmic Image')
+plt.subplot(1,4,4)
 plt.imshow(inverse_img,cmap='gray')
-plt.title('Original Image')
+plt.title('Inverse logarithmic Image')
 plt.show()
