f7df29be552c2ad6725abe86dd00351a95ffee9d
Masum Bilah <53857306+Masum-Billah1@users.noreply.github.com>
2024-01-28 17:29:06 +0600

    Update Task_4_a.py

--- Task_4_a.py
+++ Task_4_a.py
@@ -2,59 +2,63 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
-image = cv.imread('Images/filteropimg.jpg',cv.IMREAD_GRAYSCALE)
+image = cv.imread("Images/lena.jpg",0)
 image = cv.resize(image,(512,512))
 
-row,column = image.shape
-mean_of_distribution = 0
-standard_deviation = 25
-noise = np.random.normal(mean_of_distribution,standard_deviation,image.shape).astype(np.uint8)
-
-
-noise_image = cv.add(image,noise)
-
-FFT_image = np.fft.fftshift(np.fft.fft2(noise_image)) #2D DFT
-
-D0 = 10 #d0 = cut-off frequency
-filter_order = 4
-
-def gaussian_filter(FFT_image):
-    m,n = FFT_image.shape
-    gaussian_image = np.zeros((m,n))
+D0 = 10
+def gaussian(fft_image):
+    m,n = fft_image.shape
+    gaussian_filter = np.zeros((m,n))
 
     for u in range(m):
         for v in range(n):
-            D = np.sqrt((u-m/2)**2+(v-n/2)**2)
-            gaussian_image[u,v] = np.exp(-((D**2)/(2*D0**2)))
+            d = np.sqrt((u-m/2)**2+(v-n/2)**2)
+            gaussian_filter[u][v] = np.exp(-d**2/(2*D0**2))
 
-    gaussian_constant = gaussian_image*FFT_image
+    gaussian_constant = gaussian_filter*fft_image
     gaussian_image = np.abs(np.fft.ifft2(gaussian_constant))
     gaussian_image = gaussian_image/255
-    return  gaussian_image
+    return gaussian_image;
 
-def butterworth_filter(FFT_image):
-    m, n = FFT_image.shape
-    Butterworth_image = np.zeros((m, n))
+def butterworth_filter(fft_image,filter_order):
+    m,n = fft_image.shape
+    butterworth_filter = np.zeros((m,n))
     for u in range(m):
         for v in range(n):
-            D = np.sqrt((u - m / 2) ** 2 + (v - n / 2) ** 2)
-            Butterworth_image[u, v] = 1 / (1 + (D / D0) ** (2 * filter_order))
-    Butterworth_image = FFT_image * Butterworth_image
-    Butterworth_image = np.abs(np.fft.ifft2(Butterworth_image))
-    Butterworth_image = Butterworth_image / 255
-    return Butterworth_image
+            d = np.sqrt((u-m/2)**2+(v-n/2)**2)
+            butterworth_filter[u][v] = 1/(1+(d/D0)**(2*filter_order))
 
-plt.subplot(131)
-plt.imshow(image,cmap='gray')
-plt.title('Original Image')
+    butterworth_constant = butterworth_filter*fft_image
+    butterworth_image = np.abs(np.fft.ifft2(butterworth_constant))
+    butterworth_image = butterworth_image/255
+    return butterworth_image
 
-plt.subplot(132)
-plt.imshow(gaussian_filter(FFT_image),cmap='gray')
-plt.title('Gaussian Filter Image')
+mean = 0;
+deviation = 25;
+filter_order = 4
+noise = np.random.normal(mean,deviation,image.shape).astype(np.uint8)
+noisy_image = cv.add(image,noise);
+fft_image = np.fft.fftshift(np.fft.fft2(noisy_image))
 
-plt.subplot(133)
-plt.imshow(butterworth_filter(FFT_image),cmap='gray')
-plt.title('Butterworth Filter Image')
+gaussian_image = gaussian(fft_image)
+butterworth_image = butterworth_filter(fft_image,filter_order)
+
+plt.subplot(2,2,1)
+plt.imshow(image,cmap="gray")
+plt.title("Original Image")
+
+plt.subplot(2,2,2)
+plt.imshow(noisy_image,cmap="gray")
+plt.title("Noisy Image")
+
+plt.subplot(2,2,3)
+plt.imshow(gaussian_image,cmap="gray")
+plt.title("Gaussian Image")
+
+plt.subplot(2, 2, 4)
+plt.imshow(butterworth_image, cmap="gray")
+plt.title("Butterworth Image")
+
 
 plt.tight_layout()
 plt.show()
