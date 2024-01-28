6ce4dac5b9d10b01e873303caef3eed8cf08efc0
Masum Bilah <53857306+Masum-Billah1@users.noreply.github.com>
2024-01-29 01:06:32 +0600

    Open Close

--- Task_5_b.py
+++ Task_5_b.py
@@ -1,29 +1,38 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
-image = cv.imread('Images/errosion_dialation_fingerprint.png',cv.IMREAD_GRAYSCALE);
+image = cv.imread('Images/open.png',0)
+image = cv.resize(image,(512,512))
 _,binary_image = cv.threshold(image, 128,256, cv.THRESH_BINARY)
-cv.resize(image,(512,512))
 
 row,column = image.shape
-structuring_element = np.ones((3,3))
+structuring_element = np.ones((50,50))
+size = len(structuring_element)
 
-def errosion_operation(image, structuring_element):
-    erosion_image = np.copy(binary_image)
-    for i in range(1,row-1):
-        for j in range(1,column-1):
-            erosion_image[i,j] = np.min(image[i-1:i+2,j-1:j+2]*structuring_element)
-    return erosion_image
+def errosion_op(image, st_elemen):
+    pad_h = size // 2
+    padded_image = np.pad(image, pad_h, mode='constant')
+    row,col = image.shape
+    erosion_img = np.zeros_like(image)
+    for i in range(row):
+        for j in range(col):
+            tmp_window = padded_image[i:i + size, j:j + size]
+            erosion_img[i, j] = np.min(tmp_window * structuring_element)
+    return erosion_img
 
-def dialation_operation(image, structuring_element):
-    dialation_image = np.copy(binary_image)
-    for i in range(1, row-1):
-        for j in range(1, column-1):
-            dialation_image[i,j] = np.max(image[i-1:i+2,j-1:j+2]*structuring_element)
-    return dialation_image
+def dilation_op(image, st_element):
+    row,col = image.shape
+    pad_h = size // 2
+    padded_image = np.pad(image, pad_h, mode='constant')
+    dilation_img = np.zeros_like(image)
+    for i in range(row):
+        for j in range(col):
+            tmp_window = padded_image[i:i + size, j:j + size]
+            dilation_img[i, j] = np.max(tmp_window * structuring_element)
+    return dilation_img
 
-opening_img = dialation_operation(errosion_operation(image,structuring_element),structuring_element)
-closing_img = errosion_operation(dialation_operation(image,structuring_element),structuring_element)
+opening_img = dilation_op(errosion_op(binary_image,structuring_element),structuring_element)
+closing_img = errosion_op(dilation_op(binary_image,structuring_element),structuring_element)
 
 plt.subplot(131)
 plt.imshow(binary_image,cmap='gray')
